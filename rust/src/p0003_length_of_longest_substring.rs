78073a5cc0777791a7e177fbb53a42ae714322bf
Aleksandr Ustinov <ustinovas@gmail.com>
2022-11-10 10:00:48 +0000

    Add an alternative implementation of #3 problem in Rust

--- rust/src/p0003_length_of_longest_substring.rs
+++ rust/src/p0003_length_of_longest_substring.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 struct Solution;
@@ -31,6 +32,30 @@ impl Solution {
     }
 }
 
+#[allow(dead_code)]
+struct Solution1;
+impl Solution1 {
+    #[allow(dead_code)]
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut max_length = 0;
+        let mut length = 0;
+        let mut start_index = 0;
+        let mut char_indices = HashMap::new();
+        for (index, c) in s.chars().enumerate() {
+            length = match char_indices.insert(c, index) {
+                None => length + 1,
+                Some(prev_index) if prev_index < start_index => length + 1,
+                Some(prev_index) => {
+                    start_index = prev_index + 1;
+                    max_length = cmp::max(max_length, length);
+                    (index - prev_index) as i32
+                }
+            }
+        }
+        cmp::max(max_length, length)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use test_case::test_case;
@@ -40,6 +65,7 @@ mod test {
     #[test_case(String::from("bbbbb"), 1)]
     #[test_case(String::from("pwwkew"), 3)]
     fn solve(s: String, answer: i32) {
-        assert_eq!(answer, Solution::length_of_longest_substring(s));
+        assert_eq!(answer, Solution::length_of_longest_substring(s.clone()));
+        assert_eq!(answer, Solution1::length_of_longest_substring(s));
     }
 }
