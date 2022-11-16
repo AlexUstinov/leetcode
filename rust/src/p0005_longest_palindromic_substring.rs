fdea9ac0c5a0e6b76d4469a2be03fc6823ceeb05
Aleksandr Ustinov <ustinovas@gmail.com>
2022-11-16 02:32:11 +0000

    Make 'expand' a local function

--- rust/src/p0005_longest_palindromic_substring.rs
+++ rust/src/p0005_longest_palindromic_substring.rs
@@ -1,32 +1,32 @@
 struct Solution;
 
 impl Solution {
-    fn expand(s_bytes: &[u8], mut l: usize, mut r: usize) -> Option<&[u8]> {
-        if r >= s_bytes.len() || s_bytes[l] != s_bytes[r] {
-            return None;
-        }
-        loop {
-            if let (Some(new_l), Some(new_r)) = (l.checked_sub(1), r.checked_add(1)) {
-                if new_r < s_bytes.len() && s_bytes[new_l] == s_bytes[new_r] {
-                    (l, r) = (new_l, new_r);
-                    continue;
-                }
-            }
-            break Some(&s_bytes[l..(r + 1)]);
-        }
-    }
-
     pub fn longest_palindrome(s: String) -> String {
+        fn expand(s_bytes: &[u8], mut l: usize, mut r: usize) -> Option<&[u8]> {
+            if r >= s_bytes.len() || s_bytes[l] != s_bytes[r] {
+                return None;
+            }
+            loop {
+                if let (Some(new_l), Some(new_r)) = (l.checked_sub(1), r.checked_add(1)) {
+                    if new_r < s_bytes.len() && s_bytes[new_l] == s_bytes[new_r] {
+                        (l, r) = (new_l, new_r);
+                        continue;
+                    }
+                }
+                break Some(&s_bytes[l..(r + 1)]);
+            }
+        }
+
         let s_bytes = s.as_bytes();
         let mut center = 0usize;
         let mut max_palindrome: &[u8] = &[];
         while center < s_bytes.len() {
-            if let Some(max_candidate) = Solution::expand(s_bytes, center, center) {
+            if let Some(max_candidate) = expand(s_bytes, center, center) {
                 if max_palindrome.len() < max_candidate.len() {
                     max_palindrome = max_candidate;
                 }
             }
-            if let Some(max_candidate) = Solution::expand(s_bytes, center, center + 1) {
+            if let Some(max_candidate) = expand(s_bytes, center, center + 1) {
                 if max_palindrome.len() < max_candidate.len() {
                     max_palindrome = max_candidate;
                 }
