12ec33639b6b3e22561e2effea08f69129075da6
Aleksandr Ustinov <ustinovas@gmail.com>
2022-11-27 05:38:07 +0000

    Implement zig zag iterator in Rust

--- rust/src/p0006_zigzag_conversion.rs
+++ rust/src/p0006_zigzag_conversion.rs
@@ -20,23 +20,69 @@ impl<'a> ZigZagIterable for &'a str {
     type ZZIter = ZigZagIterator<'a>;
     fn zigzag_iter(self, num_rows: i32) -> Self::ZZIter {
         ZigZagIterator {
-            s: self,
+            bytes: self.as_bytes(),
             num_rows,
-            row: 0
+            row: -1,
+            idx: usize::MAX,
+            next_el: None
         }
     }    
 }
 
 struct ZigZagIterator<'a> {
-    s: &'a str,
+    bytes: &'a [u8],
     num_rows: i32,
-    row: i32
+    row: i32,
+    idx: usize,
+    next_el: Option<u8>
 }
 
 impl<'a> Iterator for ZigZagIterator<'a> {
     type Item = u8;
     fn next(&mut self) -> Option<u8> {
-        unimplemented!()
+        match self.next_el {
+            Some(el) => {
+                self.next_el = None;
+                Some(el)
+            },
+            None => {
+                let len = self.bytes.len();
+                if self.idx >= len {
+                    self.row += 1;
+                    self.idx = 0;
+                    if self.row == self.num_rows {
+                        return None;
+                    }
+                }
+                if self.row==0 {
+                    let el = self.bytes[self.idx];
+                    self.idx += (2*self.num_rows - 2) as usize;
+                    Some(el)
+                }
+                else if self.row == self.num_rows - 1 {
+                    let el_idx = self.idx + (self.row as usize);
+                    let el = if el_idx < len { Some(self.bytes[el_idx]) } else { None };
+                    self.idx += (2*self.num_rows - 2) as usize;
+                    el
+                }
+                else {
+                    let idx1 = self.idx.checked_sub(self.row as usize);
+                    let idx2 = self.idx + (self.row as usize);
+                    self.idx += (2*self.num_rows - 2) as usize;
+                    match idx1 {
+                        Some(idx1) => {
+                            if idx2 < len {
+                                self.next_el = Some(self.bytes[idx2]);
+                            }
+                            Some(self.bytes[idx1])
+                        },
+                        None => {
+                            Some(self.bytes[idx2])
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
