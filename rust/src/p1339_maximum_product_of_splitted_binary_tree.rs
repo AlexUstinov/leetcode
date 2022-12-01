8cffa1b02c376e8e83dd6a5f95224c6411756bf9
Aleksandr Ustinov <ustinovas@gmail.com>
2022-12-01 18:51:56 +0000

    Implement solution of the #1339 problem

--- rust/src/p1339_maximum_product_of_splitted_binary_tree.rs
+++ rust/src/p1339_maximum_product_of_splitted_binary_tree.rs
@@ -1,12 +1,35 @@
 use crate::binary_tree::TreeNode;
 
+use std::cmp;
 use std::rc::Rc;
 use std::cell::RefCell;
 
 pub struct Solution;
 impl Solution {
     pub fn max_product(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        
+        fn dfs<T, G>(node: &Option<Rc<RefCell<TreeNode>>>, func: &G, default: T) -> T
+            where
+                G: Fn(i64, T, T) -> T,
+                T: Copy  {
+            match node.as_deref() {
+                Some(node) => {
+                    let node = &*node.borrow();
+                    func(node.val as i64, dfs(&node.left, func, default), dfs(&node.right, func, default))
+                },
+                None => default
+            }
+        }
+        let tree_sum = dfs(&root, &|val: i64, left: i64, right: i64| val + left + right, 0i64);
+        let (_, min_diff) = dfs(&root,&|val: i64, left: (i64, i64), right: (i64, i64)| {
+            let sum = val + left.0 + right.0;
+            let diff: i64 = tree_sum - 2 * sum;
+            let min_diff = cmp::min(cmp::min(diff.abs(), left.1), right.1);
+            (sum, min_diff)
+        }, (0, i64::MAX));
+        let subtree_sum = (tree_sum - min_diff) / 2;
+        const MODULO: i64 = 1000000007;
+        (((subtree_sum % MODULO) * ((tree_sum - subtree_sum) % MODULO)) % MODULO).try_into()
+            .expect("After modulo division the value must fit")
     }
 }
 
