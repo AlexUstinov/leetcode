5af8e99d5ad70cb05abef67aa07bb96c851cd721
Aleksandr Ustinov <ustinovas@gmail.com>
2022-12-01 19:14:21 +0000

    Clean up code of the problem's #1339 solution

--- rust/src/p1339_maximum_product_of_splitted_binary_tree.rs
+++ rust/src/p1339_maximum_product_of_splitted_binary_tree.rs
@@ -5,31 +5,25 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 pub struct Solution;
+const MOD: i64 = 1_000_000_007;
 impl Solution {
     pub fn max_product(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         fn dfs<T, G>(node: &Option<Rc<RefCell<TreeNode>>>, func: &G, default: T) -> T
-            where
-                G: Fn(i64, T, T) -> T,
-                T: Copy  {
-            match node.as_deref() {
-                Some(node) => {
-                    let node = &*node.borrow();
-                    func(node.val as i64, dfs(&node.left, func, default), dfs(&node.right, func, default))
-                },
-                None => default
+            where G: Fn(i64, T, T) -> T, T: Copy
+        {
+            if let Some(node) = node {
+                let node = node.borrow();
+                return func(node.val as i64, dfs(&node.left, func, default), dfs(&node.right, func, default));
             }
+            default
         }
         let tree_sum = dfs(&root, &|val: i64, left: i64, right: i64| val + left + right, 0i64);
         let (_, min_diff) = dfs(&root,&|val: i64, left: (i64, i64), right: (i64, i64)| {
             let sum = val + left.0 + right.0;
-            let diff: i64 = tree_sum - 2 * sum;
-            let min_diff = cmp::min(cmp::min(diff.abs(), left.1), right.1);
-            (sum, min_diff)
+            (sum, cmp::min((tree_sum - 2 * sum).abs(), cmp::min(left.1, right.1)))
         }, (0, i64::MAX));
         let subtree_sum = (tree_sum - min_diff) / 2;
-        const MODULO: i64 = 1000000007;
-        (((subtree_sum % MODULO) * ((tree_sum - subtree_sum) % MODULO)) % MODULO).try_into()
-            .expect("After modulo division the value must fit")
+        (((subtree_sum % MOD) * ((tree_sum - subtree_sum) % MOD)) % MOD) as i32
     }
 }
 
