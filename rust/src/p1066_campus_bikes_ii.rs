294d3d2a206c9a0dc2b26927a8181e66cea10b15
Aleksandr Ustinov <ustinovas@gmail.com>
2022-12-14 22:16:31 +0000

    Implement bruteforce solution of problem #1066 Campus Bikes II

--- rust/src/p1066_campus_bikes_ii.rs
+++ rust/src/p1066_campus_bikes_ii.rs
@@ -1,21 +1,45 @@
-pub struct Solution;
+pub struct BruteforceSolution;
 
-impl Solution {
+impl BruteforceSolution {
     pub fn assign_bikes(workers: Vec<Vec<i32>>, bikes: Vec<Vec<i32>>) -> i32 {
-        0
+        fn distance(a: &[i32], b: &[i32]) -> i32 {
+            (a[0] - b[0]).abs() + (a[1] - b[1]).abs()
+        }
+        fn assign(assignment: i32, workers: &Vec<Vec<i32>>, bikes: &Vec<Vec<i32>>) -> i32 {
+            let worker = assignment.count_ones() as usize;
+            if worker < workers.len() {
+                let mut min_assignment = i32::MAX;
+                for (bike, bike_pos) in bikes.iter().enumerate() {
+                    let bike_mask = 1 << bike;
+                    if assignment & bike_mask == 0 {
+                        let dist_to_bike = distance(&workers[worker], bike_pos);
+                        let total_distance = dist_to_bike + assign(assignment | bike_mask, workers, bikes);
+                        min_assignment = min_assignment.min(total_distance);
+                    }
+                }
+                min_assignment
+            }
+            else {
+                0
+            }
+        }
+
+        assign(0, &workers, &bikes)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use test_case::test_case;
+    use super::*;
     use crate::util::parse_pairs;
-    use super::Solution;
+    use test_case::test_case;
 
     #[test_case("[[0,0],[2,1]]", "[[1,2],[3,3]]", 6)]
     #[test_case("[[0,0],[1,1],[2,0]]", "[[1,0],[2,2],[2,1]]", 4)]
-    fn solve(workers: &str, bikes: &str, expected: i32) {
-        assert_eq!(expected, Solution::assign_bikes(parse_pairs(workers), parse_pairs(bikes)));
+    fn solve_brutforce(workers: &str, bikes: &str, expected: i32) {
+        assert_eq!(
+            expected,
+            BruteforceSolution::assign_bikes(parse_pairs(workers), parse_pairs(bikes))
+        );
     }
-
 }
