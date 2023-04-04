edae81a27f116577ecb7307ce52830f3621ffb32
Aleksandr Ustinov <ustinovas@gmail.com>
2023-04-04 07:09:53 +0000

    Solve problem #146 'LRU cache'

---
+++ rust/src/p0146_lru_cache.rs
@@ -0,0 +1,143 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+struct ListNode {
+    key: i32,
+    val: i32,
+    prev: Option<Rc<RefCell<ListNode>>>,
+    next: Option<Rc<RefCell<ListNode>>>
+}
+
+impl ListNode {
+    fn new(key: i32, val: i32) -> Self {
+        Self { key, val, prev: None, next: None }
+    }
+}
+
+pub struct LRUCache {
+    capacity: usize,
+    head: Option<Rc<RefCell<ListNode>>>,
+    tail: Option<Rc<RefCell<ListNode>>>,
+    map: HashMap<i32, Rc<RefCell<ListNode>>>
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl LRUCache {
+
+    fn new(capacity: i32) -> Self {
+        Self {
+            capacity: capacity as usize,
+            head: None,
+            tail: None,
+            map: HashMap::new()
+        }
+    }
+    
+    fn get(&mut self, key: i32) -> i32 {
+        if let Some(node) = self.map.get_mut(&key) {
+            let ListNode { val, ref mut prev, ref mut next, .. } = &mut *node.borrow_mut();
+            let mut insert_as_head = false;
+            if let Some(prev_node) = prev {
+                let ListNode { next: ref mut prev_next, .. } = &mut *prev_node.borrow_mut();
+                if let Some(next_node) = next.as_ref() {
+                    let ListNode { prev: ref mut next_prev, ..} = &mut *next_node.borrow_mut();
+                    prev_next.insert(Rc::clone(next_node));
+                    next_prev.insert(Rc::clone(prev_node));
+                } else {
+                    *prev_next = None;
+                    self.tail.insert(Rc::clone(prev_node));
+                }
+                insert_as_head = true;
+            }
+            if insert_as_head {
+                if let Some(head_node) = self.head.as_ref() {
+                    let ListNode { prev: ref mut head_prev, .. } = &mut *head_node.borrow_mut();
+                    head_prev.insert(Rc::clone(node));
+                }
+                *next = self.head.take();
+                self.head.insert(Rc::clone(node));
+                *prev = None;
+            }
+
+            return *val;
+        }
+        -1
+    }
+    
+    fn put(&mut self, key: i32, value: i32) {
+        if self.get(key) >= 0 {
+            if let Some(node) = self.map.get_mut(&key) {
+                let ListNode { val, .. } = &mut *node.borrow_mut();
+                *val = value;
+            }
+        }
+        else {
+            let node = Rc::new(RefCell::new(ListNode::new(key, value)));
+            if let Some(head) = self.head.as_ref() {
+                let ListNode { prev: ref mut head_prev, .. } = &mut *head.borrow_mut();
+                head_prev.insert(Rc::clone(&node));
+                let ListNode { next: ref mut node_next, .. } = &mut *node.borrow_mut();
+                node_next.insert(Rc::clone(head));
+            }
+            if self.tail.is_none() {
+                self.tail = Some(Rc::clone(&node));
+            }
+            self.head = Some(Rc::clone(&node));
+            self.map.insert(key, node);
+
+            if self.map.len() > self.capacity {
+                if let Some(tail) = self.tail.take() {
+                    let ListNode { key: tail_key, prev: ref mut tail_prev, .. } = &mut *tail.borrow_mut();
+                    if let Some(new_tail) = tail_prev {
+                        let ListNode { next: ref mut new_tail_next, .. }  = &mut *new_tail.borrow_mut();
+                        *new_tail_next = None;
+                        self.tail.insert(Rc::clone(new_tail));
+                    }
+                    else {
+                        self.head = None;
+                        self.tail = None;
+                    }
+                    self.map.remove(tail_key);
+                }
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_capacity_1() {
+        let mut cache = LRUCache::new(1);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        assert_eq!(-1, cache.get(1));
+        cache.put(3, 3);
+        assert_eq!(-1, cache.get(2));
+        cache.put(4, 4);
+        assert_eq!(-1, cache.get(1));
+        assert_eq!(-1, cache.get(3));
+        assert_eq!(4, cache.get(4));
+    }
+
+    #[test]
+    fn test_capacity_2() {
+        let mut cache = LRUCache::new(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        assert_eq!(1, cache.get(1));
+        cache.put(3, 3);
+        assert_eq!(-1, cache.get(2));
+        cache.put(4, 4);
+        assert_eq!(-1, cache.get(1));
+        assert_eq!(3, cache.get(3));
+        assert_eq!(4, cache.get(4));
+    }
+}
